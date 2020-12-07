b7f65988b1831d98a8db52ec46ab03e4fcde2abc
unknown <peter.zhng1@gmail.com>
2020-12-07 18:00:36 +1100

    finished setting up react

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+function App() 
+  return (
+    <div>
+      <input type="checkbox"/>
+      <p>placeholder text</p>
+      <input type="checkbox"/>
+      <p>placeholder text</p>
+      <input type="checkbox"/>
+      <p>placeholder text</p>
+      <input type="checkbox"/>
+      <p>placeholder text</p>
+    </div>
+  )
+
+export default App
