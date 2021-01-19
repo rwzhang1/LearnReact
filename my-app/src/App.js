3b5dda2affbeb79bccf23532b31c28f570b5a185
Peter Zhang <peter.zhng1@gmail.com>
2021-01-19 17:18:45 +1100

    react reusable components tutorial

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,13 @@
 import React from "react"
-
-import Header from "./Header"
-import MainContent from "./MainContent"
-import Footer from "./Footer"
-
+import ContactCard from "./ContactCard"
 
 function App() {
   return (
-    <div>
-      <Header/>
-      <MainContent/>
-      <Footer/>
+    <div className="contacts">
+      <ContactCard contact={{name:"a", imgUrl:"https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png", phone: "1111 111 111", email: "cat@cat.cat"}}></ContactCard>
+      <ContactCard contact={{name:"b", imgUrl:"https://www.humanesociety.org/sites/default/files/styles/1240x698/public/2018/08/kitten-440379.jpg?h=c8d00152&itok=1fdekAh2", phone: "2222 222 222", email: "cat@cat.cat"}}></ContactCard>
+      <ContactCard contact={{name:"c", imgUrl:"https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png", phone: "3333 333 333", email: "cat@cat.cat"}}></ContactCard>
+      <ContactCard contact={{name:"d", imgUrl:"https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png", phone: "4444 444 444", email: "cat@cat.cat"}}></ContactCard>
     </div>
   )
 }
